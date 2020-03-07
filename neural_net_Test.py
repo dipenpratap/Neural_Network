d7d892d52aa83a0b7501684940aecc2c50588cac
Dipen Yadav <dipenyadav@Dipens-MacBook-Pro.local>
2020-03-06 21:48:20 -0600

    1st Progress

---
+++ neural_net_Test.py
@@ -0,0 +1,5 @@
+from neural_network import *
+import numpy as np
+
+model = NeuralNetwork(x=, y=1)
+model.summary()
---
+++ neural_network_keras.py
@@ -0,0 +1,6 @@
+class neural_network:
+    def __init__(self,input_shape,output_shape):
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+
+
